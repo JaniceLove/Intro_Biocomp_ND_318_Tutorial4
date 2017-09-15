b1654049f179a83d6b6d065c08b426d2ee324c49
janicelove <jlove5@nd.edu>
2017-09-15 11:36:25 -0400

    added comments to script file

--- genderYrExp.sh
+++ genderYrExp.sh
@@ -1,1 +1,5 @@
+#Script to sort wages.csv by gender, save gender and years Experience to a new file, seperated by space
+#Usage:bash genderYrExp.sh $@ where $@ is wages.csv or whatever file to be sorted 
+
+
 cat "$@" | sort -k 1| tr -d ' '| cut -d ',' -f 1,2| tr ',' ' '  > challenge1.txt
